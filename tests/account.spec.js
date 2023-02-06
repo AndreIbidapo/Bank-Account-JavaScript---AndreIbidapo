fd1110920b01dddcb644c7f6e9c9bf317af0954d
Andre Ibidapo <andreibidapo@gmail.com>
2023-02-06 09:40:47 +0000

    Formatting

--- tests/account.spec.js
+++ tests/account.spec.js
@@ -4,15 +4,25 @@ const BankAccount = require('../src/Account.js');
 const assert = require("assert");
 
 
-// Test the Transaction class
+
+
+
+
+
+//Tests
+
+// `node specRunner.js` in terminal to run
+
+(`========================================================================`);
+// Testing the Transaction class
 const transaction = new Transaction("10/01/2012", 1000.00, 0.00);
 assert.deepEqual(
     transaction,
     { date: "10/01/2012", credit: 1000.00, debit: 0.00 },
     "Transaction is not working as expected"
 );
-
-// Test the Statement class
+(`=========================================================================`);
+// Testing the Statement class
 const statement = new Statement([
     new Transaction("10/01/2012", 1000.00, 0.00),
     new Transaction("13/01/2012", 2000.00, 0.00),
@@ -25,10 +35,13 @@ assert.deepEqual(
         { date: "13/01/2012", credit: 2000.00, debit: 0.00, balance: 3000.00 },
         { date: "14/01/2012", credit: 0.00, debit: 500.00, balance: 2500.00 }
     ],
+
     "Statement is not working as expected"
 );
 
-// Test the BankAccount class
+(`====================================================================================`);
+
+// Testing the BankAccount class
 const account = new BankAccount();
 account.deposit(1000);
 account.deposit(2000);
